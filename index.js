fd3c75d06c53ee55b50ab2be9a52cc3df97361a0
nkpn <illinmikita@gmail.com>
2022-03-21 11:57:03 +0200

    second wildlife page (not ready)

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-const $jq = jQuery.noConflict();
-
+console.log('Hi! If you need a website - please contact me : illinmikita@gmail.com , or by telegram : https://t.me/nk_pn')
 $(document).ready(function(){
-    console.log('test')
+    $('.burger,.close-btn').click(function(event){
+        $('.mobile-menu').toggleClass('active')
+        
+    })
     $('.carousel').slick();
-  });
+})
+
+const wildlifeIcon = $('.wildlife');
+const wildlifeList = $('.wildlife-list');
+const moshiIcon =$('.moshi');
+const moshiList =$('.moshi-list');
+const kilimanjaroIcon = $('.kilimanjaro');
+const kilimanjaroList =$('.kilimanjaro-list');
+const zanzibarIcon = $('.zanzibar');
+const zanzibarList =$('.zanzibar-list');
+
+wildlifeIcon.click(()=>{
+    wildlifeList.toggle();
+})
+
+moshiIcon.click(()=>{
+    moshiList.toggle();
+})
+
+kilimanjaroIcon.click(()=>{
+   kilimanjaroList.toggle();
+})
+
+zanzibarIcon.click(()=>{
+    zanzibarList.toggle();
+})
