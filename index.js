bbfef9fd743d025bb52b95a2efc2008c78dcdb55
nkpn <illinmikita@gmail.com>
2022-03-21 22:17:58 +0200

    pos sticky image

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ const kilimanjaroIcon = $('.kilimanjaro');
 const kilimanjaroList =$('.kilimanjaro-list');
 const zanzibarIcon = $('.zanzibar');
 const zanzibarList =$('.zanzibar-list');
+const daysIconOpen = $('.days-arrow');
+const daysIconClose = $('.days-arrow-close');
+
+const CampOne = $('.camping-one');
+const CampOneBottom = $('.camp-one-arr-bottom');
+const CampOneUp = $('.camp-one-arr-up');
+const CampOneList= $('.camp-one-list');
+
+const CampThree = $('.camping-three');
+const CampThreeBottom = $('.camp-three-arr-bottom');
+const CampThreeUp = $('.camp-three-arr-up');
+const CampThreeList= $('.camp-three-list');
+
+const CampFour = $('.camping-four');
+const CampFourBottom = $('.camp-four-arr-bottom');
+const CampFourUp = $('.camp-four-arr-up');
+const CampFourList= $('.camp-four-list');
 
 wildlifeIcon.click(()=>{
     wildlifeList.toggle();
@@ -30,4 +47,34 @@ kilimanjaroIcon.click(()=>{
 
 zanzibarIcon.click(()=>{
     zanzibarList.toggle();
+})
+
+daysIconOpen.click(()=>{
+    daysIconOpen.parent().siblings().css("display", "block");
+    daysIconOpen.hide();
+    daysIconClose.show();
+})
+
+daysIconClose.click(()=>{
+    daysIconOpen.parent().siblings().css("display", "none");
+    daysIconClose.hide();
+    daysIconOpen.show();
+})
+
+CampOne.click(()=>{
+    CampOneBottom.toggle();
+    CampOneUp.toggle();
+    CampOneList.toggle();
+})
+
+CampThree.click(()=>{
+    CampThreeBottom.toggle();
+    CampThreeUp.toggle();
+    CampThreeList.toggle();
+})
+
+CampFour.click(()=>{
+    CampFourBottom.toggle();
+    CampFourUp.toggle();
+    CampFourList.toggle();
 })
