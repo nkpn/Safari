08a8f2f857ff2184affbe0703e9452965f9126f0
nkpn <illinmikita@gmail.com>
2022-04-12 13:20:20 +0300

    fix 3

--- js/index.js
+++ js/index.js
@@ -20,8 +20,8 @@ $(document).ready(function(){
         speed:400,
         variableWidth: true,
         appendArrows:$('.carousel-buttons'),
-        prevArrow:`<img src="../../images/icons/left-arrow.svg" alt="left">`,
-        nextArrow:`<img src="../../images/icons/right-arrow.svg" alt="right">`,
+        prevArrow:`<img src="./left-arrow.svg" alt="left">`,
+        nextArrow:`<img src="./right-arrow.svg" alt="right">`,
         responsive: [
             {
                 breakpoint: 2048,
