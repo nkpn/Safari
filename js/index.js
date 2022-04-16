3b01278ecc5047e98d03e14cce742f99edfbf30d
nkpn <illinmikita@gmail.com>
2022-04-16 11:16:04 +0300

    arrows fix

--- js/index.js
+++ js/index.js
@@ -14,20 +14,22 @@ $(document).ready(function(){
     
 
     $('.carousel').slick({
-        // infinite: true,
+        infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
         speed:400,
         variableWidth: true,
         appendArrows:$('.carousel-buttons'),
-        prevArrow:`<p class="text-arrow">←</p>`,
-        nextArrow:`<p class="text-arrow">→</p>`,
+        // prevArrow:`<p class="text-arrow">←</p>`,
+        // nextArrow:`<p class="text-arrow">→</p>`,
+        prevArrow:`<img src="../js/left-arrow.png" alt="left">`,
+        nextArrow:`<img src="../js/right-arrow.png" alt="right">`,
         responsive: [
             {
                 breakpoint: 2048,
                 settings: {
                   slidesToShow: 3,
-                  slidesToScroll: 3
+                  slidesToScroll: 1
                 }
               },
             
@@ -42,7 +44,7 @@ $(document).ready(function(){
                 breakpoint: 1024,
                 settings: {
                   slidesToShow: 2,
-                  slidesToScroll:2
+                  slidesToScroll:1
                 }
               },
             ]
@@ -62,9 +64,9 @@ $(document).ready(function(){
                 breakpoint: 2048,
                 settings: {
                   slidesToShow: 3,
-                  slidesToScroll: 3,
+                  slidesToScroll: 1,
                   infinite: true,
-                  dots: false
+                  dots: true
                 }
               },
             
@@ -79,9 +81,9 @@ $(document).ready(function(){
                 breakpoint: 1024,
                 settings: {
                   slidesToShow: 2,
-                  slidesToScroll:2,
+                  slidesToScroll:1,
                   infinite: true,
-                  dots: false
+                  dots: true
                 }
               },
             ]
