90fb0fa10bc486a90c5c517812df0c279aed21a8
nkpn <illinmikita@gmail.com>
2022-05-19 23:22:50 +0300

    1

--- js/langArrZanzibar.js
+++ js/langArrZanzibar.js
@@ -1,4 +1,476 @@
-const langArrZanzibar = {};
+const langArrZanzibar = {
+  //* header
+  "nav-home": {
+    en: "Home",
+    ua: "Головна",
+    fr: "Accueil",
+    de: "Startseite",
+  },
+  "nav-wildlife": {
+    en: "Wildlife Safari Tours",
+    ua: "Сафарі-тури дикої природи",
+    fr: "Safari de la vie sauvage",
+    de: "Wildlife Safari",
+  },
+  "nav-kilimonjaro": {
+    en: "The Kilimanjaro Mountain",
+    ua: "Гора Кіліманджаро",
+    fr: "Le mont Kilimandjaro",
+    de: "Der Berg Kilimandscharo",
+  },
+  "nav-moshi": {
+    en: "Day trips in Moshi",
+    ua: "Одноденні екскурсії в Моші",
+    fr: "Excursions d'une journée à Moshi",
+    de: "Tagesausflüge nach Moshi",
+  },
+  "nav-zanzibar": {
+    en: "The Zanzibar Archipelago",
+    ua: "Архіпелаг Занзібар",
+    fr: "L'archipel de Zanzibar",
+    de: "Das Sansibar-Archipel",
+  },
+  "nav-contact": {
+    en: "Contact Us",
+    ua: "Зв'яжіться з нами",
+    fr: "Nous contacter",
+    de: "Kontaktieren Sie uns",
+  },
+  //* subheader wildlife mobile
+  "subhead-wildlife-camping": {
+    en: "Camping Safari",
+    ua: "Кемпінг Сафарі",
+    fr: "Le safari en camping",
+    de: "Die Campingsafari",
+  },
+  "subhead-wildlife-mid": {
+    en: "Mid-Range Safari lodge",
+    ua: "Сафарі-лодж середнього класу",
+    fr: "Le safari en lodge de moyenne gamme.",
+    de: "Die Safari in einer Lodge der Mittelklasse",
+  },
+  "subhead-wildlife-wilderness": {
+    en: "The Wilderness and Cultural experience safari",
+    ua: "Сафарі по пустелі та культурному досвіду",
+    fr: "Le safari en milieu sauvage et l'expérience culturelle",
+    de: "Die Safari in der Wildnis und das kulturelle Erlebnis",
+  },
+  "subhead-wildlife-lux": {
+    en: "Luxury Lodge Safari",
+    ua: "Люкс-лодж сафарі",
+    fr: "Le safari en lodge de luxe",
+    de: "Die Safari in einer Luxus-Lodge",
+  },
+  "subhead-wildlife-classic": {
+    en: "Classic Lodge Safari",
+    ua: "Класичний лодж сафарі",
+    fr: "Le safari en lodge classique",
+    de: "Die Safari in einer klassischen Lodge",
+  },
+  //* subnav wildlife mobile
+  "sub-nav-wildlife": {
+    en: "Wildlife Safari Tours",
+    ua: "Сафарі-тури дикої природи",
+    fr: "Safari de la vie sauvage",
+    de: "Wildlife Safari",
+  },
+  "subnav-wildlife-camping": {
+    en: "Camping Safari",
+    ua: "Кемпінг Сафарі",
+    fr: "Le safari en camping",
+    de: "Die Campingsafari",
+  },
+  "subnav-wildlife-mid": {
+    en: "Mid-Range Safari lodge",
+    ua: "Сафарі-лодж середнього класу",
+    fr: "Le safari en lodge de moyenne gamme.",
+    de: "Die Safari in einer Lodge der Mittelklasse",
+  },
+  "subnav-wildlife-wilderness": {
+    en: "The Wilderness and Cultural experience safari",
+    ua: "Сафарі по пустелі та культурному досвіду",
+    fr: "Le safari en milieu sauvage et l'expérience culturelle",
+    de: "Die Safari in der Wildnis und das kulturelle Erlebnis",
+  },
+  "subnav-wildlife-lux": {
+    en: "Luxury Lodge Safari",
+    ua: "Люкс-лодж сафарі",
+    fr: "Le safari en lodge de luxe",
+    de: "Die Safari in einer Luxus-Lodge",
+  },
+  "subnav-wildlife-classic": {
+    en: "Classic Lodge Safari",
+    ua: "Класичний лодж сафарі",
+    fr: "Le safari en lodge classique",
+    de: "Die Safari in einer klassischen Lodge",
+  },
+  //* subhead moshi
+  "subhead-moshi": {
+    en: "Day trips in Moshi",
+    ua: "Одноденні екскурсії в Моші",
+    fr: "Excursions d'une journée à Moshi",
+    de: "Tagesausflüge nach Moshi",
+  },
+  "subhead-moshi-arusha": {
+    en: "Arusha National Park",
+    ua: "Національний парк Аруша",
+    fr: "Parc national d'Arusha",
+    de: "Arusha-Nationalpark",
+  },
+  "subhead-moshi-tarangire": {
+    en: "Tarangire National Park",
+    ua: "Національний парк Тарангіре",
+    fr: "Le parc national du Tarangiré",
+    de: "Der Tarangire-Nationalpark",
+  },
+  "subhead-moshi-marangu": {
+    en: "Marangu waterfalls",
+    ua: "Водоспад Марангу",
+    fr: "Les chutes d'eau de Marangu",
+    de: "Die Wasserfälle von Marangu",
+  },
+  "subhead-moshi-chemka": {
+    en: "Chemka hot springs",
+    ua: "Гарячі джерела Чемка",
+    fr: "Les sources chaudes de Chemka",
+    de: "Die heißen Quellen von Chemka",
+  },
+  "subhead-moshi-materuni": {
+    en: "Materuni waterfalls",
+    ua: "Водоспади Матеруні",
+    fr: "Les chutes d'eau de Materuni",
+    de: "Die Wasserfälle von Materuni",
+  },
+  "subhead-moshi-challa": {
+    en: "Lake Challa",
+    ua: "Озеро Чалла",
+    fr: "Le lac Challa",
+    de: "Der Challa-See",
+  },
+  "subhead-moshi-chagga": {
+    en: "Chagga museum",
+    ua: "Музей Чагга",
+    fr: "Le musée Chagga",
+    de: "Das Chagga-Museum",
+  },
+  //* subnav moshi mobile
+  "sub-nav-moshi": {
+    en: "Day trips in Moshi",
+    ua: "Одноденні екскурсії в Моші",
+    fr: "Excursions d'une journée à Moshi",
+    de: "Tagesausflüge nach Moshi",
+  },
+  "subnav-moshi-arusha": {
+    en: "Arusha National Park",
+    ua: "Національний парк Аруша",
+    fr: "Parc national d'Arusha",
+    de: "Arusha-Nationalpark",
+  },
+  "subnav-moshi-tarangire": {
+    en: "Tarangire National Park",
+    ua: "Національний парк Тарангіре",
+    fr: "Le parc national du Tarangiré",
+    de: "Der Tarangire-Nationalpark",
+  },
+  "subnav-moshi-marangu": {
+    en: "Marangu waterfalls",
+    ua: "Водоспад Марангу",
+    fr: "Les chutes d'eau de Marangu",
+    de: "Die Wasserfälle von Marangu",
+  },
+  "subnav-moshi-chemka": {
+    en: "Chemka hot springs",
+    ua: "Гарячі джерела Чемка",
+    fr: "Les sources chaudes de Chemka",
+    de: "Die heißen Quellen von Chemka",
+  },
+  "subnav-moshi-materuni": {
+    en: "Materuni waterfalls",
+    ua: "Водоспади Матеруні",
+    fr: "Les chutes d'eau de Materuni",
+    de: "Die Wasserfälle von Materuni",
+  },
+  "subnav-moshi-challa": {
+    en: "Lake Challa",
+    ua: "Озеро Чалла",
+    fr: "Le lac Challa",
+    de: "Der Challa-See",
+  },
+  "subnav-moshi-chagga": {
+    en: "Chagga museum",
+    ua: "Музей Чагга",
+    fr: "Le musée Chagga",
+    de: "Das Chagga-Museum",
+  },
+  //* subhead kilimanjaro
+  "subnav-kilimanjaro": {
+    en: "Marangu Routes",
+    ua: "Маршрути Марангу",
+    fr: "Routes Marangu",
+    de: "Marangu-Straßen",
+  },
+  "subhead-kil-marangu": {
+    en: "Marangu Routes",
+    ua: "Маршрути Марангу",
+    fr: "Routes Marangu",
+    de: "Marangu-Straßen",
+  },
+  "subhead-kil-machame": {
+    en: "Machame Route",
+    ua: "Маршрут Мачаме",
+    fr: "Route Machame",
+    de: "Straße Machame",
+  },
+  "subhead-kil-rongai": {
+    en: "Rongai Route",
+    ua: "Маршрут Ронгай",
+    fr: "Route Rongai",
+    de: "Straße Rongai",
+  },
+  "subhead-kil-lemosho": {
+    en: "Lemosho Route",
+    ua: "Маршрут Лемошо",
+    fr: "Lemosho Route",
+    de: "Der Berg Kilimandscharo",
+  },
+  "subhead-kil-umbwe": {
+    en: "Umbwe Route",
+    ua: "Маршрут Умбве",
+    fr: "Circuit Umbwe",
+    de: "Umbwe-Rundreise",
+  },
+  "subhead-kil-north": {
+    en: "Northern Circuit",
+    ua: "Північний ланцюг",
+    fr: "Circuit Nord",
+    de: "Nord-Rundreise",
+  },
+  "subhead-kil-meru": {
+    en: "Trekking Mount Meru",
+    ua: "Трекінг на гору Меру",
+    fr: "Trekking du Mont Meru",
+    de: "Mount Meru Trekking",
+  },
+  //* subnav mobile kilimanjaro
+  "subnav-kil-marangu": {
+    en: "Marangu Routes",
+    ua: "Маршрути Марангу",
+    fr: "Routes Marangu",
+    de: "Marangu-Straßen",
+  },
+  "subnav-kil-machame": {
+    en: "Machame Route",
+    ua: "Маршрут Мачаме",
+    fr: "Route Machame",
+    de: "Straße Machame",
+  },
+  "subnav-kil-rongai": {
+    en: "Rongai Route",
+    ua: "Маршрут Ронгай",
+    fr: "Route Rongai",
+    de: "Straße Rongai",
+  },
+  "subnav-kil-lemosho": {
+    en: "Lemosho Route",
+    ua: "Маршрут Лемошо",
+    fr: "Lemosho Route",
+    de: "Der Berg Kilimandscharo",
+  },
+  "subnav-kil-umbwe": {
+    en: "Umbwe Route",
+    ua: "Маршрут Умбве",
+    fr: "Circuit Umbwe",
+    de: "Umbwe-Rundreise",
+  },
+  "subnav-kil-north": {
+    en: "Northern Circuit",
+    ua: "Північний ланцюг",
+    fr: "Circuit Nord",
+    de: "Nord-Rundreise",
+  },
+  "subnav-kil-meru": {
+    en: "Trekking Mount Meru",
+    ua: "Трекінг на гору Меру",
+    fr: "Trekking du Mont Meru",
+    de: "Mount Meru Trekking",
+  },
+  //* subheader zanzibar
+  "subhead-zanzibar-tarangire": {
+    en: "Tarangire National Park",
+    ua: "Національний парк Тарангіре",
+    fr: "Parc national du Tarangiré",
+    de: "Tarangire-Nationalpark",
+  },
+  "subhead-zanzibar-mtowambue": {
+    en: "Mto wa Mbu to Manyara National Park",
+    ua: "Гора ва Мбу до національного парку Маньяра",
+    fr: "Du parc national de Mto wa Mbu au parc national de Manyara",
+    de: "Vom Mto wa Mbu-Nationalpark zum Manyara-Nationalpark",
+  },
+  "subhead-zanzibar-ngorongoro": {
+    en: "Ngorongoro crater",
+    ua: "Кратер Нгоронгоро",
+    fr: "Le cratère du Ngorongoro",
+    de: "Der Ngorongoro-Krater",
+  },
+  "subhead-zanzibar-beach": {
+    en: "Zanzibar beach",
+    ua: "Пляж Занзібару",
+    fr: "La plage de Zanzibar",
+    de: "Der Strand von Sansibar",
+  },
+  "subhead-zanzibar-stone": {
+    en: "Zanzibar beach to Stone town",
+    ua: "Пляж Занзібару до Кам'яного міста",
+    fr: "De la plage de Zanzibar à Stone town",
+    de: "Vom Strand von Sansibar nach Stone town",
+  },
+
+  //* subnav zanzibar
+  "subnav-zanzibar": {
+    en: "The Kilimanjaro Mountain",
+    ua: "Гора Кіліманджаро",
+    fr: "Le mont Kilimandjaro",
+    de: "Der Berg Kilimandscharo",
+  },
+  "subnav-zanzibar-tarangire": {
+    en: "Tarangire National Park",
+    ua: "Національний парк Тарангіре",
+    fr: "Parc national du Tarangiré",
+    de: "Tarangire-Nationalpark",
+  },
+  "subnav-zanzibar-mtowambue": {
+    en: "Mto wa Mbu to Manyara National Park",
+    ua: "Гора ва Мбу до національного парку Маньяра",
+    fr: "Du parc national de Mto wa Mbu au parc national de Manyara",
+    de: "Vom Mto wa Mbu-Nationalpark zum Manyara-Nationalpark",
+  },
+  "subnav-zanzibar-ngorongoro": {
+    en: "Ngorongoro crater",
+    ua: "Кратер Нгоронгоро",
+    fr: "Le cratère du Ngorongoro",
+    de: "Der Ngorongoro-Krater",
+  },
+  "subnav-zanzibar-beach": {
+    en: "Zanzibar beach",
+    ua: "Пляж Занзібару",
+    fr: "La plage de Zanzibar",
+    de: "Der Strand von Sansibar",
+  },
+  "subnav-zanzibar-stone": {
+    en: "Zanzibar beach to Stone town",
+    ua: "Пляж Занзібару до Кам'яного міста",
+    fr: "De la plage de Zanzibar à Stone town",
+    de: "Vom Strand von Sansibar nach Stone town",
+  },
+  "subnav-contact": {
+    en: "Contact Us",
+    ua: "Зв'яжіться з нами",
+    fr: "Nous contacter",
+    de: "Kontaktieren Sie uns",
+  },
+  "subhead-contact": {
+    en: "Contact Us",
+    ua: "Зв'яжіться з нами",
+    fr: "Nous contacter",
+    de: "Kontaktieren Sie uns",
+  },
+  //* Footer
+  "contact-us-h3": {
+    en: "Contact us",
+    ua: "Зв'яжіться з нами",
+    fr: "Nous contacter",
+    de: "Kontaktiere uns",
+  },
+  "label-name": {
+    en: "Enter your name",
+    ua: "Введіть ім'я",
+    fr: "Entrez votre nom",
+    de: "Gib deinen Namen ein",
+  },
+  "label-email": {
+    en: "Enter your email",
+    ua: "Введіть свою електронну адресу",
+    fr: "Entrer votre Email",
+    de: "Geben sie ihre E-Mail Adresse ein",
+  },
+  "label-textarea": {
+    en: "Comment or question",
+    ua: "Коментар або запитання",
+    fr: "Commentaire ou question",
+    de: "Kommentar oder Frage",
+  },
+  "label-submit": {
+    en: "Send",
+    ua: "Надіслати",
+    fr: "Envoyer",
+    de: "Schicken",
+  },
+  "contact-phone": {
+    en: "phone",
+    ua: "телефон",
+    fr: "téléphoner",
+    de: "Telefon",
+  },
+  "contact-social": {
+    en: "social",
+    ua: "соціальні посилання",
+    fr: "Liens sociaux",
+    de: "soziale Verbindungen",
+  },
+  "contact-address": {
+    en: "address",
+    ua: "адреса",
+    fr: "adresse",
+    de: "die Anschrift",
+  },
+  "footer-wildlife": {
+    en: "Wildlife Safari Tours",
+    ua: "Сафарі-тури дикої природи",
+    fr: "Safari de la vie sauvage",
+    de: "Wildlife Safari",
+  },
+  "footer-kilimanjaro": {
+    en: "The Kilimanjaro Mountain",
+    ua: "Гора Кіліманджаро",
+    fr: "Le mont Kilimandjaro",
+    de: "Der Berg Kilimandscharo",
+  },
+  "footer-moshi": {
+    en: "Day trips in Moshi",
+    ua: "Одноденні екскурсії в Моші",
+    fr: "Excursions d'une journée à Moshi",
+    de: "Tagesausflüge nach Moshi",
+  },
+  "footer-zanzibar": {
+    en: "The Zanzibar Archipelago",
+    ua: "Архіпелаг Занзібар",
+    fr: "L'archipel de Zanzibar",
+    de: "Das Sansibar-Archipel",
+  },
+  //-----------------------------
+  //* Hero
+  "hero-h1": {
+    en: "Day trips </br>Moshi",
+    ua: "Одноденні екскурсії</br> в Моші",
+    fr: "Excursions d'une</br> journée à Moshi",
+    de: "Tagesausflüge</br> nach Moshi",
+  },
+  "hero-p1": {
+    en: "Tanzania, the world's last major wildlife sanctuary, is a perfect destination for your holiday. In the Moshi and Arusha regions, there are numerous local activities to enjoy. Tours can be organized to visit the villages, coffee plantations, waterfalls, the Moshi forest, canoeing at Momela and Duluti Lakes, Maasai settlements, hot springs, Kilimanjaro, climbing, biking, shopping at local markets, and much more! These activities can be tailored to clients' budgets, interests, and fitness levels.",
+    ua: "Танзанія, останній великий у світі заповідник дикої природи, є ідеальним місцем для вашого відпочинку. У регіонах Моші та Аруша можна насолодитися численними місцевими розвагами. Можна організувати тури, щоб відвідати села, кавові плантації, водоспади, ліс Моші, катання на каное на озерах Момела і Дулуті, поселення масаї, гарячі джерела, Кіліманджаро, скелелазіння, їзда на велосипеді, шопінг на місцевих ринках та багато іншого! Ці заходи можна адаптувати до бюджету, інтересів та рівня фізичної підготовки клієнтів.",
+    fr: "La Tanzanie, dernière grande réserve naturelle du monde, est une destination parfaite pour vos vacances. Dans les régions de Moshi et d'Arusha, vous pourrez profiter de nombreuses activités locales. Des circuits peuvent être organisés pour visiter les villages, les plantations de café, les chutes d'eau, la forêt de Moshi, faire du canoë sur les lacs Momela et Duluti, les campements masaï, les sources d'eau chaude, le Kilimandjaro, l'escalade, le vélo, le shopping sur les marchés locaux, et bien plus encore ! Ces activités peuvent être adaptées au budget, aux intérêts et au niveau de forme physique des clients.",
+    de: "Tansania, das letzte große Naturschutzgebiet der Welt, ist ein perfektes Ziel für Ihren Urlaub. In den Regionen Moshi und Arusha können Sie viele lokale Aktivitäten genießen. Es können Touren arrangiert werden, um Dörfer, Kaffeeplantagen, Wasserfälle, den Moshi-Wald, Kanufahrten auf den Seen Momela und Duluti, Masai-Camps, heiße Quellen, den Kilimanjaro, Klettern, Radfahren, Einkaufen auf lokalen Märkten und vieles mehr zu besuchen! Diese Aktivitäten können auf das Budget, die Interessen und das Fitnessniveau des Kunden zugeschnitten werden.",
+  },
+
+  "hero-book-btn": {
+    en: "Book the tour",
+    ua: "Забронюйте тур",
+    fr: "Réservez la visite",
+    de: "Buchen die Tour",
+  },
+  //---------------------------
+};
 
 //---------------------------------------------
 //* Function
