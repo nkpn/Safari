4478c2bd3f63a777d9c9a97c0c0f7b1ec451f46e
nkpn <illinmikita@gmail.com>
2022-05-20 00:15:30 +0300

    f

--- js/langArrZanzibar.js
+++ js/langArrZanzibar.js
@@ -451,18 +451,23 @@ const langArrZanzibar = {
   //-----------------------------
   //* Hero
   "hero-h1": {
-    en: "Day trips </br>Moshi",
-    ua: "Одноденні екскурсії</br> в Моші",
-    fr: "Excursions d'une</br> journée à Moshi",
-    de: "Tagesausflüge</br> nach Moshi",
+    en: "Zanzibar Archipelago",
+    ua: "Архіпелаг Занзібар",
+    fr: "Zanzibar (Archipel)",
+    de: "Sansibar (Archipel)",
   },
   "hero-p1": {
-    en: "Tanzania, the world's last major wildlife sanctuary, is a perfect destination for your holiday. In the Moshi and Arusha regions, there are numerous local activities to enjoy. Tours can be organized to visit the villages, coffee plantations, waterfalls, the Moshi forest, canoeing at Momela and Duluti Lakes, Maasai settlements, hot springs, Kilimanjaro, climbing, biking, shopping at local markets, and much more! These activities can be tailored to clients' budgets, interests, and fitness levels.",
-    ua: "Танзанія, останній великий у світі заповідник дикої природи, є ідеальним місцем для вашого відпочинку. У регіонах Моші та Аруша можна насолодитися численними місцевими розвагами. Можна організувати тури, щоб відвідати села, кавові плантації, водоспади, ліс Моші, катання на каное на озерах Момела і Дулуті, поселення масаї, гарячі джерела, Кіліманджаро, скелелазіння, їзда на велосипеді, шопінг на місцевих ринках та багато іншого! Ці заходи можна адаптувати до бюджету, інтересів та рівня фізичної підготовки клієнтів.",
-    fr: "La Tanzanie, dernière grande réserve naturelle du monde, est une destination parfaite pour vos vacances. Dans les régions de Moshi et d'Arusha, vous pourrez profiter de nombreuses activités locales. Des circuits peuvent être organisés pour visiter les villages, les plantations de café, les chutes d'eau, la forêt de Moshi, faire du canoë sur les lacs Momela et Duluti, les campements masaï, les sources d'eau chaude, le Kilimandjaro, l'escalade, le vélo, le shopping sur les marchés locaux, et bien plus encore ! Ces activités peuvent être adaptées au budget, aux intérêts et au niveau de forme physique des clients.",
-    de: "Tansania, das letzte große Naturschutzgebiet der Welt, ist ein perfektes Ziel für Ihren Urlaub. In den Regionen Moshi und Arusha können Sie viele lokale Aktivitäten genießen. Es können Touren arrangiert werden, um Dörfer, Kaffeeplantagen, Wasserfälle, den Moshi-Wald, Kanufahrten auf den Seen Momela und Duluti, Masai-Camps, heiße Quellen, den Kilimanjaro, Klettern, Radfahren, Einkaufen auf lokalen Märkten und vieles mehr zu besuchen! Diese Aktivitäten können auf das Budget, die Interessen und das Fitnessniveau des Kunden zugeschnitten werden.",
+    en: "Zanzibar, also known as Unguja, is the most mesmerizing and fascinating island in Africa.",
+    ua: "Занзібар, також відомий як Унгуджа, є найбільш зачаровуючим і захоплюючим островом в Африці.",
+    fr: "Zanzibar, également connue sous le nom d'Unguja, est l'île la plus fascinante d'Afrique.",
+    de: "Sansibar, auch bekannt als Unguja, ist Afrikas faszinierendste Insel.",
   },
-
+  "hero-p2": {
+    en: "The best destinations on the island are Stonetown, Spice Tour, Safari Blue Tour, Nungwi, Kwendwa, the east coast (Jambiani, Paje Bwejuu, and Kizimkazi). The best day trips are: Stone Town Tour, Spice Tour, Safari Blue Tour, Mnemba Atoll, Dolpion Tour, Prison Island, Jozani Forest, Sunset Dhow Cruise, Sandbank Picnic, and Mangapwani Slave Chambers.",
+    ua: "Найкращими напрямками на острові є Стоунтаун, Спайс-тур, Сафарі-Блакитний тур, Нунгві, Квендва, східне узбережжя (Джамбіані, Падже Бведжуу і Кізімказі). Найкращі одноденні екскурсії: екскурсія по Кам’яному місту, тур зі спеціями, тур Safari Blue, атол Мнемба, тур Долпіон, острів в’язниці, ліс Джозані, круїз на заході доу, пікнік на пісочній березі та камери рабів Мангапвані.",
+    fr: "Les meilleures destinations sur l'île sont Stonetown, Spice Tour, Safari Blue Tour, Nungwi, Kwendwa, la côte est (Jambiani, Paje Bwejuu, et Kizimkazi). Les meilleures excursions d'une journée sont : Stone Town Tour, Spice Tour, Safari Blue Tour, l'atoll de Mnemba, Dolpion Tour, Prison Island, Jozani Forest, Sunset Dhow Cruise, Sandbank Picnic, et Mangapwani Slave Chambers.",
+    de: "Die besten Ziele auf der Insel sind Stonetown, Spice Tour, Safari Blue Tour, Nungwi, Kwendwa, Ostküste (Jambiani, Paje Bwejuu und Kizimkazi). Die besten Tagesausflüge sind: Stone Town Tour, Spice Tour, Safari Blue Tour, Mnemba Atoll, Dolpion Tour, Prison Island, Jozani Forest, Sunset Dhow Cruise, Sandbank Picknick und Mangapwani Slave Chambers.",
+  },
   "hero-book-btn": {
     en: "Book the tour",
     ua: "Забронюйте тур",
@@ -470,6 +475,125 @@ const langArrZanzibar = {
     de: "Buchen die Tour",
   },
   //---------------------------
+  "hero2-p": {
+    en: "We can arrange the most appropriate itineraries to meet the majority of needs. We specialize in low-cost Zanzibar tour packages that include safari and beach activities. The schedule below is an example of a Bush to Beach safari or vice versa. If you want something different, please tell us your breakdown and we will gladly organize it for you.",
+    ua: "Ми можемо організувати найбільш відповідні маршрути для задоволення більшості потреб. Ми спеціалізуємося на недорогих турах на Занзібар, які включають сафарі та пляжні заходи. Розклад нижче є прикладом сафарі з Буша до пляжу або навпаки. Якщо ви хочете чогось іншого, повідомте нам свою розбивку, і ми з радістю організуємо це для вас.",
+    fr: "Nous pouvons organiser les itinéraires les plus appropriés pour répondre à la majorité des besoins. Nous sommes spécialisés dans les voyages organisés à bas prix à Zanzibar qui comprennent des safaris et des activités de plage. Le programme ci-dessous est un exemple d'un safari Bush to Beach ou vice versa. Si vous souhaitez quelque chose de différent, veuillez nous indiquer votre répartition et nous nous ferons un plaisir de l'organiser pour vous.",
+    de: "Wir können die am besten geeigneten Routen organisieren, um die meisten Bedürfnisse zu erfüllen. Wir sind spezialisiert auf kostengünstige Pauschalreisen in Sansibar, die Pirschfahrten und Strandaktivitäten beinhalten. Das folgende Programm ist ein Beispiel für eine Busch-zu-Strand-Safari oder umgekehrt. Wenn Sie etwas anderes wünschen, teilen Sie uns bitte Ihre Panne mit und wir werden es gerne für Sie arrangieren.",
+  },
+  //* tarangire
+  day1: {
+    en: "DAY 1",
+    ua: "ДЕНЬ 1",
+    fr: "JOUR 1:",
+    de: "TAG 1:",
+  },
+  "tarangire-h2": {
+    en: "Tarangire National Park",
+    ua: "Національний парк Тарангіре",
+    fr: "Parc national du Tarangiré",
+    de: "Tarangire-Nationalpark",
+  },
+  "tarangire-p": {
+    en: "After breakfast, you will be collected by your guide/driver, and your safari will begin. Drive to Tarangire National Park from Moshi or Arusha. Tarangire, as previously said, is home to a plethora of migratory wildlife species that may be found in the Tarangire River's permanent waters. You'll have a picnic lunch in the park after the half-day game drive. You'll have to drive to Mto wa Mbu's campsite/lodge in the late afternoon for dinner and an overnight stay.",
+    ua: "Після сніданку вас забере ваш гід/водій, і ваше сафарі розпочнеться. Поїздка до національного парку Тарангіре з Моші або Аруши. Тарангір, як було сказано раніше, є домом для безлічі мігруючих видів дикої природи, які можна знайти в постійних водах річки Тарангір. Після півдня ігрової поїздки ви матимете обід на пікніку в парку. Пізнього дня вам доведеться їхати до кемпінгу/лоджі Mto wa Mbu на вечерю та ночівлю.",
+    fr: "Après le petit-déjeuner, vous serez pris en charge par votre guide/chauffeur, et votre safari commencera. Route vers le parc national de Tarangire depuis Moshi ou Arusha. Tarangire, comme nous l'avons dit précédemment, abrite une pléthore d'espèces sauvages migratrices que l'on peut trouver dans les eaux permanentes de la rivière Tarangire. Vous prendrez un déjeuner pique-nique dans le parc après la demi-journée de safari. En fin d'après-midi, vous devrez vous rendre au camping/lodge de Mto wa Mbu pour le dîner et la nuit.",
+    de: "Nach dem Frühstück werden Sie von Ihrem Guide/Fahrer abgeholt und Ihre Pirschfahrt beginnt. Fahren Sie von Moshi oder Arusha zum Tarangire-Nationalpark. Tarangire ist, wie wir bereits gesagt haben, die Heimat einer Vielzahl von wandernden Wildtieren, die in den permanenten Gewässern des Tarangire-Flusses zu finden sind. Nach der halbtägigen Pirschfahrt haben Sie ein Picknick im Park. Am Ende des Nachmittags müssen Sie zum Abendessen und zur Übernachtung zum Campingplatz / zur Lodge von Mto wa Mbu gehen.",
+  },
+  //* mtowambu
+  day2: {
+    en: "DAY 2",
+    ua: "ДЕНЬ 2",
+    fr: "JOUR 2:",
+    de: "TAG 2:",
+  },
+  "mtowambu-h3": {
+    en: "From Mto wa Mbu to the Manyara National Park",
+    ua: "Від гори ва Мбу до національного парку Маньяра",
+    fr: "De Mto wa Mbu au parc national de Manyara",
+    de: "Von Mto wa Mbu zum Manyara-Nationalpark",
+  },
+  "mtowambu-p": {
+    en: "On this day, you will depart and start out with a game drive in Lake Manyara National Park. After enjoying a picnic lunch, you will continue on your game drive. In this national park, the afternoon is the best time to view animals. Zebras, impalas, monkeys, giraffes, buffalos, hippos, and warthogs are among the many animals that may be found in Lake Manyara National Park. You will return to the campsite/lodge before dark for dinner and an overnight stay.",
+    ua: "Цього дня ви вирушите і почнете з ігрової прогулянки в національному парку озера Маньяра. Насолодившись обідом на пікніку, ви продовжите свій ігровий драйв. У цьому національному парку найкращий час для споглядання тварин у другій половині дня. Зебри, імпалі, мавпи, жирафи, буйволи, бегемоти та бородавочники є серед багатьох тварин, які можна знайти в національному парку озера Маньяра. Ви повернетеся в кемпінг/лоджі до настання темряви для вечері та ночівлі.",
+    fr: "Ce jour-là, vous partirez et commencerez par un safari dans le parc national du lac Manyara. Après avoir dégusté un déjeuner pique-nique, vous poursuivrez votre safari. Dans ce parc national, l'après-midi est le meilleur moment pour observer les animaux. Zèbres, impalas, singes, girafes, buffles, hippopotames et phacochères font partie des nombreux animaux que l'on peut trouver dans le parc national du lac Manyara. Vous retournerez au camping/lodge avant la nuit pour le dîner et la nuit.",
+    de: "An diesem Tag brechen Sie auf und beginnen mit einer Pirschfahrt im Lake Manyara National Park. Nach einem Picknick-Mittagessen setzen Sie Ihre Pirschfahrt fort. In diesem Nationalpark ist der Nachmittag die beste Zeit, um die Tiere zu beobachten. Zebras, Impalas, Affen, Giraffen, Büffel, Flusspferde und Warzenschweine gehören zu den vielen Tieren, die im Lake Manyara National Park zu finden sind. Sie kehren vor Einbruch der Dunkelheit zum Abendessen und zur Übernachtung zum Campingplatz/zur Lodge zurück.",
+  },
+  //* ngorongoro
+  day3: {
+    en: "DAY 3",
+    ua: "ДЕНЬ 3",
+    fr: "JOUR 3:",
+    de: "TAG 3:",
+  },
+  "ngorongoro-h3": {
+    en: "From Ngorongoro crater to Zanzibar Island",
+    ua: "Від кратера Нгоронгоро до острова Занзібар",
+    fr: "Du cratère du Ngorongoro à l'île de Zanzibar",
+    de: "Vom Ngorongoro-Krater zur Insel Sansibar",
+  },
+  "ngorongoro-p1": {
+    en: "Early in the morning, drive down into Ngorongoro Crater and enjoy a half-day game drive. This is undoubtedly one of the best places to spot the rare black rhino in Africa, not to mention the pride of lions and the majestic black-maned lions. There are many colorful flamingos and a variety of other water birds. Leopards, cheetahs, hyenas, numerous antelope species, and various small mammals are also common in the crater.",
+    ua: "Рано вранці спустіться в кратер Нгоронгоро і насолоджуйтеся ігровою поїздкою на півдня. Це, безсумнівно, одне з найкращих місць, де можна побачити рідкісного чорного носорога в Африці, не кажучи вже про прайд левів і величних чорногривих левів. Є багато різнокольорових фламінго та безліч інших водоплавних птахів. У кратері також поширені леопарди, гепарди, гієни, численні види антилоп і різні дрібні ссавці.",
+    fr: "Tôt le matin, descendez dans le cratère du Ngorongoro et profitez d'une demi-journée de safari. C'est sans doute l'un des meilleurs endroits pour observer le rare rhinocéros noir en Afrique, sans parler de la fierté des lions et des majestueux lions à crinière noire. Il y a beaucoup de flamants roses colorés et une variété d'autres oiseaux aquatiques. Léopards, guépards, hyènes, de nombreuses espèces d'antilopes et divers petits mammifères sont également communs dans le cratère.",
+    de: "Steigen Sie am frühen Morgen in den Ngorongoro-Krater hinab und genießen Sie eine halbtägige Pirschfahrt. Dies ist wohl einer der besten Orte, um das seltene Spitzmaulnashorn in Afrika zu sehen, ganz zu schweigen vom Stolz der Löwen und majestätischen Schwarzmähnenlöwen. Es gibt viele bunte Flamingos und eine Vielzahl anderer Wasservögel. Leoparden, Geparden, Hyänen, viele Antilopenarten und verschiedene kleine Säugetiere sind im Krater ebenfalls verbreitet.",
+  },
+  "ngorongoro-p2": {
+    en: "Lunch will be served at the picnic area inside the crater floor. In the afternoon, you will drive to Arusha airport to catch your flight straight to Zanzibar Island. On arrival at the Zanzibar airport, you will meet our representative who will drive you to the hotel for dinner and an overnight stay.",
+    ua: "Обід буде подано в зоні для пікніка всередині кратера. У другій половині дня ви поїдете в аеропорт Аруші, щоб встигнути на рейс прямо на острів Занзібар. Після прибуття в аеропорт Занзібару ви зустрінете нашого представника, який відвезе вас до готелю на вечерю та ночівлю.",
+    fr: "Le déjeuner sera servi sur l'aire de pique-nique située au fond du cratère. Dans l'après-midi, vous vous rendrez à l'aéroport d'Arusha pour prendre votre vol direct pour l'île de Zanzibar. À votre arrivée à l'aéroport de Zanzibar, vous rencontrerez notre représentant qui vous conduira à l'hôtel pour le dîner et la nuit.",
+    de: "Das Mittagessen wird am Picknickplatz am Fuße des Kraters serviert. Am Nachmittag fahren Sie zum Flughafen von Arusha, um Ihren Direktflug auf die Insel Sansibar zu nehmen. Bei Ihrer Ankunft am Flughafen Sansibar treffen Sie unseren Vertreter, der Sie zum Abendessen und zur Übernachtung ins Hotel bringt.",
+  },
+  "ngorongoro-p2-copy": {
+    en: "Lunch will be served at the picnic area inside the crater floor. In the afternoon, you will drive to Arusha airport to catch your flight straight to Zanzibar Island. On arrival at the Zanzibar airport, you will meet our representative who will drive you to the hotel for dinner and an overnight stay.",
+    ua: "Обід буде подано в зоні для пікніка всередині кратера. У другій половині дня ви поїдете в аеропорт Аруші, щоб встигнути на рейс прямо на острів Занзібар. Після прибуття в аеропорт Занзібару ви зустрінете нашого представника, який відвезе вас до готелю на вечерю та ночівлю.",
+    fr: "Le déjeuner sera servi sur l'aire de pique-nique située au fond du cratère. Dans l'après-midi, vous vous rendrez à l'aéroport d'Arusha pour prendre votre vol direct pour l'île de Zanzibar. À votre arrivée à l'aéroport de Zanzibar, vous rencontrerez notre représentant qui vous conduira à l'hôtel pour le dîner et la nuit.",
+    de: "Das Mittagessen wird am Picknickplatz am Fuße des Kraters serviert. Am Nachmittag fahren Sie zum Flughafen von Arusha, um Ihren Direktflug auf die Insel Sansibar zu nehmen. Bei Ihrer Ankunft am Flughafen Sansibar treffen Sie unseren Vertreter, der Sie zum Abendessen und zur Übernachtung ins Hotel bringt.",
+  },
+  //* chemka
+  day4: {
+    en: "DAY 4",
+    ua: "ДЕНЬ 4",
+    fr: "JOUR 4:",
+    de: "TAG 4:",
+  },
+  "chemka-h3": {
+    en: "Zanzibar beach",
+    ua: "Пляж Занзібару",
+    fr: "Plage de Zanzibar",
+    de: "Strand von Sansibar",
+  },
+  "chemka-p1": {
+    en: "Spend a few days relaxing on Zanzibar beaches and, if desired, participate in some water sports and other activities offered by your hotel or lodge.",
+    ua: "Проведіть кілька днів, відпочиваючи на пляжах Занзібару, і, за бажанням, візьміть участь у деяких водних видах спорту та інших заходах, які пропонує ваш готель чи будиночок.",
+    fr: "Passez quelques jours à vous détendre sur les plages de Zanzibar et, si vous le souhaitez, participez aux sports nautiques et autres activités proposées par votre hôtel ou votre lodge.",
+    de: "Verbringen Sie ein paar entspannte Tage an den Stränden von Sansibar und nehmen Sie, wenn Sie möchten, an den Wassersport- und anderen Aktivitäten teil, die Ihr Hotel oder Ihre Lodge anbietet.",
+  },
+  //* stone
+  day5: {
+    en: "DAY 5",
+    ua: "ДЕНЬ 5",
+    fr: "JOUR 5:",
+    de: "TAG 5:",
+  },
+  "stone-h3": {
+    en: "From Zanzibar's beaches to Stone Town",
+    ua: "Від пляжів Занзібару до Кам’яного міста",
+    fr: "Des plages de Zanzibar à Stone Town",
+    de: "Von den Stränden von Sansibar bis nach Stone Town",
+  },
+  "stone-p1": {
+    en: "On this day you may take an optional Stone Town Tour. On your way there you can take the famous spice tour, led by an extremely experienced and informative guide. ",
+    ua: "У цей день ви можете здійснити додаткову екскурсію по Кам’яному місту. По дорозі туди ви можете здійснити знаменитий тур зі спеціями під керівництвом надзвичайно досвідченого та інформативного гіда. ",
+    fr: "Ce jour-là, vous pouvez faire une visite facultative de Stone Town. En chemin, vous pourrez faire le fameux tour des épices, mené par un guide extrêmement expérimenté et instructif. ",
+    de: "An diesem Tag können Sie an einer optionalen Tour durch Stone Town teilnehmen. Unterwegs können Sie an der berühmten Gewürztour teilnehmen, die von einem äußerst erfahrenen und informativen Führer geleitet wird. ",
+  },
+  "stone-p2": {
+    en: "This knowledgeable guide will sample some of the finest spices and herbs and discuss their culinary, cosmetic, and medicinal applications. You will then get the opportunity to sample the majority of the tropical fruits grown on the island, depending on the season. After that, make your way to Stone Town and check into your hotel. Spend the rest of the afternoon exploring Zanzibar's historical town.",
+    ua: "Цей досвідчений посібник спробує деякі з найкращих спецій і трав і обговорить їх кулінарне, косметичне та лікувальне застосування. Тоді ви отримаєте можливість спробувати більшість тропічних фруктів, вирощених на острові, залежно від сезону. Після цього вирушайте до Стоун-Тауна та заселяйтеся у свій готель. Проведіть решту дня, досліджуючи історичне місто Занзібару.",
+    fr: "Ce guide expérimenté vous fera goûter les épices et les herbes les plus fines et vous parlera de leurs applications culinaires, cosmétiques et médicinales. Vous aurez ensuite l'occasion de goûter à la majorité des fruits tropicaux cultivés sur l'île, en fonction de la saison. Ensuite, vous vous rendrez à Stone Town et vous vous enregistrerez à votre hôtel. Passez le reste de l'après-midi à explorer la ville historique de Zanzibar.",
+    de: "Dieser sachkundige Führer gibt Ihnen einen Vorgeschmack auf feinste Gewürze und Kräuter und informiert Sie über ihre kulinarischen, kosmetischen und medizinischen Anwendungen. Sie haben dann die Möglichkeit, je nach Jahreszeit den Großteil der auf der Insel angebauten tropischen Früchte zu probieren. Anschließend fahren Sie nach Stone Town und checken in Ihr Hotel ein. Verbringen Sie den Rest des Nachmittags damit, die historische Stadt Sansibar zu erkunden.",
+  },
 };
 
 //---------------------------------------------
