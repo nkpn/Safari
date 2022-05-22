140e9969584c2a3dac583394db69356de2ef465d
nkpn <illinmikita@gmail.com>
2022-05-22 18:29:18 +0300

    fix the subnav top function

--- js/subheader-moshi.js
+++ js/subheader-moshi.js
@@ -1,20 +1,21 @@
 $(document).ready(function () {
-    $(document).on("scroll", onScroll);
+  $(document).on("scroll", onScroll);
 });
 
-function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    $('.nav-submenu a').each(function () {
-        var currLink = $(this);
-        
-        var refElement = $(currLink.attr("href"));
-        
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('.nav-submenu li a').removeClass("active-section");
-            currLink.addClass("active-section");
-        }
-        else{
-            currLink.removeClass("active-section");
-        }
-    });
+function onScroll(event) {
+  var scrollPos = $(document).scrollTop();
+  $(".nav-submenu a").each(function () {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+
+    if (
+      refElement.position().top <= scrollPos &&
+      refElement.position().top + refElement.height() > scrollPos
+    ) {
+      $(".nav-submenu li a").removeClass("active-section");
+      currLink.addClass("active-section");
+    } else {
+      currLink.removeClass("active-section");
+    }
+  });
 }
